d42d9afd21eaea30970539d49e59ded7a1dad680
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-17 20:05:49 +0800

    test: create test should_change_to_location_0_0_W_when_executeCommand_give_0_0_South_and_command_TurnRight

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -112,4 +112,15 @@ class MarsRoverTest {
         Assertions.assertEquals(0 , currentLocation.getY());
         Assertions.assertEquals(Direction.EAST , currentLocation.getDirection());
     }
+    @Test
+    void should_change_to_location_0_0_W_when_executeCommand_give_0_0_South_and_command_TurnRight() {
+        //Given
+        Location initialLocation = new Location(0, 0, Direction.SOUTH);
+        // When
+        Location currentLocation = getCurrentLocation(initialLocation,commandTurnRight);
+        //Then
+        Assertions.assertEquals(0 , currentLocation.getX());
+        Assertions.assertEquals(0 , currentLocation.getY());
+        Assertions.assertEquals(Direction.WEST , currentLocation.getDirection());
+    }
 }
