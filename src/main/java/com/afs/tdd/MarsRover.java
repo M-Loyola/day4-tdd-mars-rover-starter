c73462983fc04958be5a5d1b2669d79f7a1e48ab
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-17 21:44:18 +0800

    refactor: fix indention

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -9,44 +9,37 @@ public class MarsRover {
     }
 
     public void executeCommandMove() {
-        if(location.getDirection() == Direction.NORTH){
+        if (location.getDirection() == Direction.NORTH) {
             location.setY(location.getY() + 1);
-        }
-        if(location.getDirection() == Direction.SOUTH){
+        } else if (location.getDirection() == Direction.SOUTH) {
             location.setY(location.getY() - 1);
-        }
-        if(location.getDirection() == Direction.EAST){
+        } else if (location.getDirection() == Direction.EAST) {
             location.setX(location.getX() + 1);
-        }
-        if(location.getDirection() == Direction.WEST){
+        } else if (location.getDirection() == Direction.WEST) {
             location.setX(location.getX() - 1);
         }
     }
+
     public void executeCommandLeft() {
-        if(location.getDirection() == Direction.NORTH){
+        if (location.getDirection() == Direction.NORTH) {
             location.setDirection(Direction.WEST);
-        }
-        else if(location.getDirection() == Direction.SOUTH){
+        } else if (location.getDirection() == Direction.SOUTH) {
             location.setDirection(Direction.EAST);
-        }
-        else if(location.getDirection() == Direction.EAST){
+        } else if (location.getDirection() == Direction.EAST) {
             location.setDirection(Direction.NORTH);
-        }
-        else if(location.getDirection() == Direction.WEST){
+        } else if (location.getDirection() == Direction.WEST) {
             location.setDirection(Direction.SOUTH);
         }
     }
+
     public void executeCommandRight() {
-        if(location.getDirection() == Direction.NORTH){
+        if (location.getDirection() == Direction.NORTH) {
             location.setDirection(Direction.EAST);
-        }
-        else if(location.getDirection() == Direction.SOUTH){
+        } else if (location.getDirection() == Direction.SOUTH) {
             location.setDirection(Direction.WEST);
-        }
-        else if(location.getDirection() == Direction.EAST){
+        } else if (location.getDirection() == Direction.EAST) {
             location.setDirection(Direction.SOUTH);
-        }
-        else if(location.getDirection() == Direction.WEST){
+        } else if (location.getDirection() == Direction.WEST) {
             location.setDirection(Direction.NORTH);
         }
     }
